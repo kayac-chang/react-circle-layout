bcfa6d57d730aff99b6ce1039ec924063d9c2199
kayacchang <egg734631@gmail.com>
2021-03-31 16:13:19 +0800

    add example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-import { ExampleComponent } from 'react-circle-layout'
-import 'react-circle-layout/dist/index.css'
+import { CircleLayout, Radian } from "react-circle-layout";
+import "react-circle-layout/dist/index.css";
+
+import IMG from "./kirby-alien.png";
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+  const [radian, setRadian] = useState(0);
 
-export default App
+  useEffect(() => {
+    const loop = () => {
+      setRadian((radian) => radian + 0.01);
+
+      requestAnimationFrame(loop);
+    };
+
+    requestAnimationFrame(loop);
+  }, []);
+
+  return (
+    <div
+      style={{
+        width: `100vw`,
+        height: `100vh`,
+        overflow: "hidden",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <CircleLayout
+        radius={24}
+        style={{
+          border: `dashed white 2px`,
+          borderRadius: "50%",
+        }}
+      >
+        <Radian radian={radian}>
+          <img
+            src={IMG}
+            alt={IMG}
+            style={{
+              width: "8rem",
+              height: "8rem",
+            }}
+          />
+        </Radian>
+      </CircleLayout>
+    </div>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,84 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import React, {
+  ReactNode,
+  CSSProperties,
+  Children,
+  useContext,
+  createContext,
+} from "react";
 
-interface Props {
-  text: string
+function transform(rad: number, radius: number) {
+  const { sin, cos } = Math;
+
+  return [
+    `translate(-50%, -50%)`,
+    `translate(${cos(rad) * radius}rem, ${sin(rad) * radius}rem)`,
+  ].join(" ");
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const LayoutContext = createContext({ radius: 0 });
+
+function useLayoutContext() {
+  const context = useContext(LayoutContext);
+
+  if (!context) {
+    throw new Error(
+      `compound components cannot be rendered outside the CircleLayout component`
+    );
+  }
+
+  return context;
+}
+
+type RadianProps = {
+  radian: number;
+  children: ReactNode;
+};
+export function Radian({ radian, children }: RadianProps) {
+  const { radius } = useLayoutContext();
+
+  return (
+    <div
+      style={{
+        transform: transform(radian, radius),
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+interface CircleLayoutProps {
+  radius: number;
+  children: ReactNode | ReactNode[];
+  style?: CSSProperties;
+}
+export function CircleLayout({
+  radius = 0,
+  children,
+  style,
+}: CircleLayoutProps) {
+  const center: CSSProperties = {
+    position: `absolute`,
+    top: `50%`,
+    left: `50%`,
+  };
+
+  return (
+    <div
+      style={{
+        ...style,
+        position: "relative",
+        width: `${radius * 2}rem`,
+        height: `${radius * 2}rem`,
+      }}
+    >
+      <LayoutContext.Provider value={{ radius }}>
+        {Children.map(children, (el, index) => (
+          <div style={center} key={index}>
+            {el}
+          </div>
+        ))}
+      </LayoutContext.Provider>
+    </div>
+  );
 }
